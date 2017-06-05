d6026d04f0de9e2aa21d3797e7cf0d9523b747c8
Dan Casas <dan.casas@gmail.com>
2017-06-05 11:19:42 +0200

    im2mp4.sh now accepts path as a parameter

--- im2mp4.sh
+++ im2mp4.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-ffmpeg -r 25 -f image2 -pattern_type glob -i 'render/*.png' -vcodec libx264 -crf 25 -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"  -pix_fmt y
-uv420p test.mp4
+cat $1/*.png | ffmpeg -f image2pipe -r 25 -i - -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -pix_fmt yuv420p -crf 25 test.mp4
